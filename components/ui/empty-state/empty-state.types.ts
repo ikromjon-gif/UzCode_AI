2bb63ddc7572c4fbab54e175d98ea5e854d977ac
IKROMJON <ikromjonkorealife@gmail.com>
2026-08-03 19:03:47 +0900

    Rename 'title' to 'heading' in EmptyStateProps

--- components/ui/empty-state/empty-state.types.ts
+++ components/ui/empty-state/empty-state.types.ts
@@ -2,7 +2,7 @@ import type * as React from "react";
 
 export interface EmptyStateProps extends React.HTMLAttributes<HTMLDivElement> {
   icon?: React.ReactNode;
-  title: React.ReactNode;
+  heading: React.ReactNode;
   description?: React.ReactNode;
   action?: React.ReactNode;
 }
